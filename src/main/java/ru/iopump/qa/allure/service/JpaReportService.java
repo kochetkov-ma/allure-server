84bd7b43965b8038be60885c69be9680c5f80cda
grusanov <grusanov@adsterra.com>
2020-11-09 03:08:39 +0300

    up pmd version and delete unused import

--- src/main/java/ru/iopump/qa/allure/service/JpaReportService.java
+++ src/main/java/ru/iopump/qa/allure/service/JpaReportService.java
@@ -19,7 +19,6 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
-import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
