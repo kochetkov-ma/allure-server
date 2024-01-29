650449cc83a150e23989218b59e76d7d2d602ba1
kochetkov-ma <kochetkov-ma@yandex.ru>
2024-01-29 12:06:12 +0000

    - fix pmd

--- src/main/java/ru/iopump/qa/allure/security/BasicConfiguration.java
+++ src/main/java/ru/iopump/qa/allure/security/BasicConfiguration.java
@@ -1,6 +1,5 @@
 package ru.iopump.qa.allure.security;
 
-import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
