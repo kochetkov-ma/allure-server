d725aab34611b0c707a71e27a5d6ecbb4876a3ba
kochetkov-ma <apmatypa88@gmail.com>
2024-08-15 11:05:50 +0100

    fix: #90 Error creating bean with name 'allureServerPlugins' defined in class path resource [ru/iopump/qa/allure/config/SpringConfiguration.class]

--- src/main/java/ru/iopump/qa/allure/config/SpringConfiguration.java
+++ src/main/java/ru/iopump/qa/allure/config/SpringConfiguration.java
@@ -7,6 +7,7 @@ import ru.iopump.qa.allure.helper.plugin.AllureServerPlugin;
 import ru.iopump.qa.util.ReflectionUtil;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Slf4j
 @Configuration
@@ -14,9 +15,14 @@ public class SpringConfiguration {
 
     @Bean
     public Collection<AllureServerPlugin> allureServerPlugins() {
-        var plugins = ReflectionUtil.createImplementations(AllureServerPlugin.class, null);
-        log.info("[ALLURE SERVER CONFIGURATION] Allure server plugins loaded: {}", plugins.stream().map(SpringConfiguration::name).toList());
-        return plugins;
+        try {
+            var plugins = ReflectionUtil.createImplementations(AllureServerPlugin.class, null);
+            log.info("[ALLURE SERVER CONFIGURATION] Allure server plugins loaded: {}", plugins.stream().map(SpringConfiguration::name).toList());
+            return plugins;
+        } catch (Throwable throwable) {
+            log.error("Failed to load allure server plugins. No plugins will be applied", throwable);
+            return Collections.emptyList();
+        }
     }
 
     private static String name(AllureServerPlugin plugin) {
